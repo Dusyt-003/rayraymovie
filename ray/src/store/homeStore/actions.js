6a6c65b65fab358317ef79ef427d3b056bd7e9da
SuperdoingB <741699599@qq.com>
2020-03-05 14:32:12 +0800

    删除了ray

--- ray/src/store/homeStore/actions.js
+++
@@ -1,9 +0,0 @@
-export const actions = {
-
-    //获取新片榜数据
-    getNewMovieData(context, result) {
-      //提交mutations
-      context.commit('getNewMovieData', result);
-    }
-  
-  }
--- ray/src/store/movieDetailStore/actions.js
+++
@@ -1,5 +0,0 @@
-export const actions = {
-  getMovieDetailData(context, data) {
-    context.commit('getMovieDetailData', data);
-  }
-};
